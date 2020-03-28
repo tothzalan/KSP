661588c319e68bf4ecd1b73b0caf5a9b0b31b300
Zalan <toth.zalan03@gmail.com>
2020-03-28 16:26:16 +0000

    Added styling

--- main.cpp
+++ main.cpp
@@ -2,6 +2,34 @@
 #include<vector>
 #include<fstream>
 #include<stdio.h>
+#include<string.h>
+#include<tuple>
+
+struct Att {
+	attr_t normal = A_NORMAL;
+	attr_t bold = A_BOLD;
+	attr_t underline = A_UNDERLINE;
+};
+
+struct Text {
+	std::string value;
+	attr_t attr;
+};
+
+std::tuple<std::string, attr_t> translate(std::string raw_text){
+	Att atrs;
+	std::tuple<std::string, attr_t> tmp;
+	if(raw_text.substr(0,3).compare("#s#") == 0)
+		tmp = std::make_tuple(raw_text.substr(3,raw_text.size()), atrs.bold);	
+	else if(raw_text.substr(0,3).compare("#u#") == 0)
+		tmp = std::make_tuple(raw_text.substr(3,raw_text.size()), atrs.underline);
+	else if(raw_text.substr(0,3).compare("#l#") == 0)
+		tmp = std::make_tuple("\t -> "+raw_text.substr(3,raw_text.size()), A_DIM);
+	else
+		tmp = std::make_tuple(raw_text, atrs.normal);	
+	
+	return tmp;
+}
 
 std::vector<std::string> read_in(std::string filename){
 	std::vector<std::string> tmp;
@@ -13,7 +41,6 @@ std::vector<std::string> read_in(std::string filename){
 	}
 	return tmp;
 }
-
 int main(int argc, char **argv){
 	initscr();
 	keypad(stdscr,true);
@@ -26,8 +53,12 @@ int main(int argc, char **argv){
 			clear();
 			std::vector<std::string> lines = read_in(argv[index]);
 			for(int i = 0; i <  lines.size(); i++){
-				printw(lines[i].c_str());
+				std::tuple<std::string, attr_t> tmp = translate(lines[i]);
+				Text line_txt {std::get<0>(tmp), std::get<1>(tmp)};
+				attron(line_txt.attr);
+				printw(line_txt.value.c_str());
 				printw("\n");
+				attroff(line_txt.attr);
 			}
 			int inp;
 			inp = getch();
